2c7888c41efd84b938303cad4730a615f06ae71b
Kristine Erickson <ericksonk09@gmail.com>
2016-01-26 09:24:29 -0500

    makeChange function

--- coin-changer/coin.js
+++ coin-changer/coin.js
@@ -1,12 +1,16 @@
+function makeChange(value) {
+  var change = [];
+  var coins = [25, 10, 5, 1];
 
+  for (var i = 0; i <= coins.length; i++) {
+    var numCoins = Math.floor(value / coins[i]);
 
-// The coin changer is a simple kata. Build a function that will return an array of USD change [Quarters, Dimes, Nickels, Pennies] for any given integer.
-//
-// make_change(145) #=> [25, 25, 25, 25, 25, 10, 10]
-// make_change(100) #=> [25, 25, 25, 25]
-// make_change(75)  #=> [25, 25, 25]
-// make_change(80)  #=> [25, 25, 25, 5]
-// make_change(70)  #=> [25, 25, 10, 10]
-// make_change(62)  #=> [25, 25, 10, 1, 1]
-// make_change(4)   #=> [1, 1, 1, 1]
-// make_change(0)   #=> []
+    if (value >= coins[i]) {
+      for (var j = 0; j < numCoins; j++) {
+        change.push(coins[i]);
+      }
+    }
+    value = value - (numCoins * coins[i]);
+  }
+  return change;
+}
